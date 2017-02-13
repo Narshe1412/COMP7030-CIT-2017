60505064724ef5451199d63284bb3a42c6e46c9d
Manuel Colorado <manuel1412@gmail.com>
2017-02-13 19:44:07 +0000

    lecture example, arraylist implementation

---
+++ W03 ADT/src/myList.java
@@ -0,0 +1,31 @@
+
+public interface myList {
+	
+	//-------------------------------------------------------------------
+	// Basic Operation --> Create an empty myList: my_create_empty
+	//-------------------------------------------------------------------
+		//public myList my_create_empty(); --> Java does not support constructors in interfaces. Use instead:
+											   //myListArray()	
+											   //myListArrayList()
+			
+	//-------------------------------------------------------------------
+	// Basic Operation --> Get number of integers in myList: my_get_length
+	//-------------------------------------------------------------------	
+	public int my_get_length();
+	
+	//-------------------------------------------------------------------
+	// Basic Operation --> Get integer of myList at a concrete index: my_get_element
+	//-------------------------------------------------------------------
+	public int my_get_element(int index);
+		
+	//-------------------------------------------------------------------
+	// Basic Operation --> Add integer to myList at a concrete index: my_add_element 
+	//-------------------------------------------------------------------
+	public void my_add_element(int index, int value);
+	
+	//-------------------------------------------------------------------
+	// Basic Operation --> Remove index of myList: my_remove_element 
+	//-------------------------------------------------------------------	
+	public void my_remove_element(int index);
+	
+}
