274501971d564494b85eb4bc98c62710ee96d4f5
Manuel Colorado <manuel1412@gmail.com>
2017-02-15 10:08:47 +0000

    my own stack interface

---
+++ W03 Stack Interface/src/myStack.java
@@ -0,0 +1,11 @@
+
+public interface myStack {
+	//TODO documentation for user
+	public boolean push(int num);
+	
+	//TODO documentation again
+	public int pop();
+	
+	public String printArray();
+	
+}
---
+++ W03 Stack Interface/src/myStackArray.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+
+public class myStackArray implements myStack{
+
+	private Integer[] myArray;
+	private int index;
+	
+	public myStackArray(int size){
+		//constructor
+		this.myArray = new Integer[size];
+		this.index = 0;
+	}
+	
+	public boolean push(int num) {
+		try {
+			if (isFull()) {
+				this.myArray = resize(this.myArray);
+			}
+			this.myArray[index] = num;
+			this.index += 1;
+		} catch (Exception e){
+			System.out.println("There was a problem adding " + num + " to the stack.");
+			return false;
+		}
+		return true;
+	}
+	
+	public int pop() {
+		int value=-1;
+		if(isEmpty()) {
+			System.out.println("Array is empty, cannot remove more elements.");
+		} else {
+			this.index -= 1;
+			value= this.myArray[this.index];
+		}
+		return value;
+
+	}
+	
+	private Integer[] resize(Integer[] original) {
+		Integer[] newArray = new Integer[(int) Math.round(original.length * 1.5)];
+		for (int i = 0; i< original.length; i++) 
+			newArray[i] = original[i];
+		return newArray;
+	}
+	
+	private boolean isFull(){
+		return this.index >= this.myArray.length;
+	}
+	
+	private boolean isEmpty(){
+		return this.index == 0;
+	}
+
+public String printArray(){
+	return Arrays.toString(this.myArray) + " size: " +  this.index;
+}
+}
