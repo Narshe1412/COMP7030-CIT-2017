0465ebde49af0b1b2dca3d23ccad32381da98cf1
Manuel Colorado <manuel1412@gmail.com>
2017-02-20 18:32:19 +0000

    lectures from week 04

---
+++ W04 Linked List/src/myListArrayList.java
@@ -0,0 +1,68 @@
+
+import java.util.ArrayList;
+
+public class myListArrayList implements myList {
+
+	//--------------------------------------------------
+	// Attributes
+	//--------------------------------------------------
+	private ArrayList<Integer> items; 
+
+	//-------------------------------------------------------------------
+	// Basic Operation --> Create an empty myList: my_create_empty
+	//-------------------------------------------------------------------
+	//public myList my_create_empty(){}
+
+	public myListArrayList(){
+		this.items = new ArrayList<Integer>();
+	}
+		
+	//-------------------------------------------------------------------
+	// Basic Operation --> Get number of integers in myList: my_get_length
+	//-------------------------------------------------------------------	
+	public int my_get_length(){
+		return this.items.size();
+	}
+	
+	//-------------------------------------------------------------------
+	// Basic Operation --> Get integer of myList at a concrete index: my_get_element
+	//-------------------------------------------------------------------
+	public int my_get_element(int index){
+		int value = 0;
+		
+		try{			
+			value = this.items.get(index);		
+		}
+		catch (Exception e){
+			System.out.println("Sorry, not such an index to access");
+		}
+		
+		return value;	
+	}
+	
+	//-------------------------------------------------------------------
+	// Basic Operation --> Add integer to myList at a concrete index: my_add_element 
+	//-------------------------------------------------------------------
+	public void my_add_element(int index, int value){
+		try{
+			this.items.add(index, value);		
+		}
+		catch (Exception e){
+			System.out.println("Sorry, not such an index to add");
+		}		
+	}
+	
+	//-------------------------------------------------------------------
+	// Basic Operation --> Remove index of myList: my_remove_element 
+	//-------------------------------------------------------------------	
+	public void my_remove_element(int index){
+		try{			
+			this.items.remove(index);		
+		}
+		catch (Exception e){
+			System.out.println("Sorry, not such an index to remove");
+		}		
+	}
+	
+}
+
